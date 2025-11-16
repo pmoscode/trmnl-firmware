8b8f9aa99d3cfd3a1761f647f6d3d0ad6949784b
Peter Motzko <info@pmoscode.de>
2025-11-16 16:28:20 +0100

    chore: set base url

--- include/config.h
+++ include/config.h
@@ -95,6 +95,6 @@ enum WIFI_CONNECT_RETRY_TIME // Time to sleep before trying to connect to the Wi
 
 #define SERVER_MAX_RETRIES 3
 
-#define API_BASE_URL "https://trmnl.app"
+#define API_BASE_URL "https://trmnl.pmsys.zone"
 
 #endif
